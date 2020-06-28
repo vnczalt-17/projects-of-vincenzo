be1f2175041eeb27f6c426361c6bdfcef7648cfb
Vincenzo <vncz.17@gmail.com>
2020-06-28 22:13:36 +0300

    python-workshop-last-version

--- python-workshop/hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python-workshop/hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -8,6 +8,6 @@ def hello():
 
 if __name__=='__main__':
     # app.run('localhost', port=5000, debug=True)  default values
-     app.run(debug=True) to run the app. via your local
+     app.run(debug=True) # to run the app. via your local
     # app.run('0.0.0.0', port=80)  to run the app. via EC2, activate this line
 
