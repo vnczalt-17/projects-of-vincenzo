720774e7e75152a6c3f17e3b3dc831d2e173da97
Vincenzo <vncz.17@gmail.com>
2020-06-28 20:11:08 +0300

    app

--- python-workshop/hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python-workshop/hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -11,5 +11,5 @@ print('name of this app..:', __name__)
 if __name__ == '__main__':
 	#app.run('localhost', port=5000, debug=True)>>default values
 	app.run(debug=True)
-	#app.run('0.0.0.0', port=80)
+	
 	
