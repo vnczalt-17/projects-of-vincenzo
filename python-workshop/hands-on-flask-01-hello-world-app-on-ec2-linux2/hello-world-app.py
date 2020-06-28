f50cac6354f96ab09988e715b365d5e2e7d9295e
Vincenzo <vncz.17@gmail.com>
2020-06-28 22:09:10 +0300

    python-workshop-last-version

--- python-workshop/hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python-workshop/hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Hello World from Vincenzo"
+    return 'Hello World from Call'
 
-print('name of this app..:', __name__)
+if __name__=='__main__':
+    # app.run('localhost', port=5000, debug=True)  default values
+     app.run(debug=True) to run the app. via your local
+    # app.run('0.0.0.0', port=80)  to run the app. via EC2, activate this line
 
-if __name__ == '__main__':
-	#app.run('localhost', port=5000, debug=True)>>default values
-	app.run(debug=True)
-	
-	
