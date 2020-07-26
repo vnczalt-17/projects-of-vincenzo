35538b817a52f83a106147c31b505f416d92b25c
Vincenzo <vncz.17@gmail.com>
2020-07-26 16:26:42 +0300

    ms converter first try

--- aws-workshop/001-roman-numerals-converter/app.py
+++ aws-workshop/001-roman-numerals-converter/app.py
@@ -31,5 +31,5 @@ def main_post():
     return render_template('result.html', developer_name='İrfan İskender', number_decimal=number, number_roman=convert(number))
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
--- python-workshop/hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
+++ python-workshop/hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
@@ -6,9 +6,9 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'workshopdb.c1uttz89srhs.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'call-mysql-db-server.cbanmzptkrzf.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Sql_guy1234'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
@@ -107,5 +107,5 @@ def add_email():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-    app.run(debug=True)
-   #app.run(host='0.0.0.0', port=80)
+#    app.run(debug=True)
+   app.run(host='0.0.0.0', port=80)
